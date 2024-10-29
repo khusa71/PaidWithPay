635365b592b1579f0e878992d69b5d19f7e2c0a4
khusa71 <khusa71@gmail.com>
2024-10-29 15:06:08 +0000

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PaidWithPay/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
